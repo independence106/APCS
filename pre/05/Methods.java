03aac18e539c8d8df9a4eea8a95777995bc2f41e
independence106 <independence106@gmail.com>
2021-09-27 18:53:19 -0400

    Add files via upload

---
+++ pre/05/Methods.java
@@ -0,0 +1,9 @@
+public class Methods{
+	public static void zool(int number, String string1, String string2) {
+		System.out.println(number + " " + string1 + " " + string2);
+	}
+	public static void main(String[] args) {
+		zool(11, "no pet", "Independence Drive");
+	}
+	
+}
