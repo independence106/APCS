76fce456de743ec132d8d1299772c8bf5b164061
Jason Zhou <jzhou33@stuy.edu>
2021-10-01 14:06:23 -0400

    did some stuff :D

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,21 @@
+public class BigSib {
+        /*
+        public static void main(String[] args) {
+                //scanner for fun
+                
+                Scanner scan = new Scanner(System.in);
+                System.out.println("What's your name?");
+                String name = scan.nextLine();
+                greeterInput(name);
+                
+
+                //greeterPersonalized("Why hello there friend. This is not the first time we have met. \n I am constantly watching you forever and ever.");
+        }
+        */
+        
+        public static void greeterPersonalized(String string) {
+                System.out.println(string);
+
+        }
+        public static void greeterInput(String string) {
+                System.out.println("Why hello there " + string + ". This is not the first time we have met. \n I am constantly watching you forever and ever.");
---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;  // Import the Scanner class
+
+public class Greet extends BigSib {
+        public static void main(String[] args) {
+                //scanner for fun
+		/*
+                Scanner scan = new Scanner(System.in);
+                System.out.println("What's your name?");
+                String name = scan.nextLine();
+                greeterInput(name);
+		*/
+
+                BigSib.greeterPersonalized("E");
+        }
+        
+}
