77616d522a2db51d408b2dcc267238eb8bdf2d52
Jason Zhou <jzhou33@stuy.edu>
2021-10-01 21:43:58 -0400

    fixed some items

--- Greet.java
+++
@@ -1,9 +0,0 @@
-public class Greet {
-	public static void main(String[] args) {
-		greeterPersonalized();
-	}
-	public void greeterPersonalized() {
-		System.out.println("Why hello there friend. This is not the first time we have met. \n I am constantly watching you forever and ever.");
-		
-	}
-}
