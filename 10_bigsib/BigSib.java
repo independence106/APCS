115d47d07e7536c3340ba13c0f64ae0b797c710b
Jason Zhou <jzhou33@stuy.edu>
2021-10-04 21:02:56 -0400

    did HW10

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,39 @@
+/* 
+   Jason Zhou
+   APCS
+   HW10: Refactor Big Sib One (Make Method greet w/ parameter string and returns string)
+   2021-10-04
+
+   Discoveries: n/a
+   Unresolved Questions: n/a
+*/
+
+public class BigSib {
+    /*
+    public static void main(String[] args) {
+            //scanner for fun
+            
+            Scanner scan = new Scanner(System.in);
+            System.out.println("What's your name?");
+            String name = scan.nextLine();
+            greeterInput(name);
+            
+
+            //greeterPersonalized("Why hello there friend. This is not the first time we have met. \n I am constantly watching you forever and ever.");
+    }
+    */
+    static String nameOfSib;
+    public BigSib() {
+            nameOfSib = "";
+    }
+    public BigSib(String name) {
+            nameOfSib = name;
+    }
+    
+    public static String greet(String string) {
+        return string;
+    }
+    
+    
+    
+}
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,19 @@
+/* 
+   Jason Zhou
+   APCS
+   HW10: Refactor Big Sib One (Make Method greet w/ parameter string and returns string)
+   2021-10-04
+
+   Discoveries: n/a
+   Unresolved Questions: n/a
+*/
+public class Greet {
+    public static void main(String[] args) {
+
+            System.out.println(BigSib.greet("Jonathon"));           
+            System.out.println(BigSib.greet("Elmo"));          
+            System.out.println(BigSib.greet("Buddy"));
+
+    }
+    
+}
