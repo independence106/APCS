062eb51062fc3ea342f1036f676ebc35c4d17fec
Jason Zhou <jzhou33@stuy.edu>
2021-11-19 13:59:20 -0500

    e

---
+++ YoRPG/Monster.java
@@ -0,0 +1,53 @@
+public class Monster {
+
+  private int health;
+  private int damage;
+  private String attackState = "";
+  private String monsterName = "";
+  private int level;
+
+
+  public Monster() {
+    this.health = 100;
+    this.damage = 15;
+    this.level = 0;
+    this.attackState = "normalize";
+  }
+  public Monster(String name) {
+    this();
+    this.monsterName = name;
+  }
+  public String getName() {
+    return this.monsterName;
+  }
+
+  public String toString() {
+    return "Name: " + getName() + "\nLevel: " + this.level + "\nHealth: " + this.health + "\nAttack State" + this.attackState;
+  }
+
+  public boolean isAlive() {
+    return health >= 0;
+  }
+  public void takeDamage(int damage) {
+    this.health -= damage;
+  }
+  public int attack(Protagonist name) {
+    if (attackState.equals("specialize")) {
+      //System.out.println("Swinging Hammer...");
+      int amplifier = (int) (Math.random() * 10);
+      if (amplifier > 5) {
+        damage += amplifier;
+      } else {
+        System.out.println("Youg swung too hard, and bashed thy skull");
+        takeDamage(5);
+      }
+    } else {
+      damage = 15;
+    }
+    name.takeDamage(damage);
+
+
+    return damage;
+  }
+
+}
---
+++ YoRPG/Protagonist.java
@@ -0,0 +1,58 @@
+public class Protagonist {
+
+  private int health;
+  private int damage;
+  private String attackState = "";
+  private String protagonistName = "";
+  private int level;
+
+
+  public Protagonist() {
+    this.health = 100;
+    this.damage = 15;
+    this.level = 0;
+    this.attackState = "normalize";
+  }
+  public Protagonist(String name) {
+    this();
+    this.protagonistName = name;
+  }
+  public String getName() {
+    return this.protagonistName;
+  }
+
+  public String toString() {
+    return "Name: " + getName() + "\nLevel: " + this.level + "\nHealth: " + this.health + "\nAttack State" + this.attackState;
+  }
+  public void specialize() {
+    this.attackState = "specialize";
+  }
+  public void normalize() {
+    this.attackState = "normalize";
+  }
+  public boolean isAlive() {
+    return health >= 0;
+  }
+  public void takeDamage(int damage) {
+    this.health -= damage;
+  }
+  public int attack(Monster name) {
+    if (attackState.equals("specialize")) {
+      //System.out.println("Swinging Hammer...");
+      int amplifier = (int) (Math.random() * 10);
+      if (amplifier > 2) {
+        damage = 15 + amplifier;
+      } else {
+        System.out.println("Youg swung too hard, and bashed thy skull");
+        damage = 0;
+        takeDamage(5);
+      }
+    } else {
+      damage = 15;
+    }
+    name.takeDamage(damage);
+
+    return damage;
+  }
+
+}
