d1f6d70979e999b71d386d2b5a755c72ffa0ca6f
Jason Zhou <jzhou33@stuy.edu>
2022-04-04 13:08:15 -0400

    e

---
+++ 88_rqueue/RQueue.java
@@ -0,0 +1,112 @@
+/***
+ * class RQueue
+ * SKELETON
+ * A node-based, randomized queue
+ * (a collection with FIIDKO property)
+ *
+ *       -------------------------------
+ *   end |  --->   Q U E U E   --->    | front
+ *       -------------------------------
+ *
+ *  linkages point opposite direction for O(1) en/dequeuing
+ *            N <- N <- ... <- N <- N
+ *      _end -^                     ^- _front
+ *
+ **/
+
+
+public class RQueue<SWASHBUCKLE> implements Queue<SWASHBUCKLE>
+{
+  //instance variables
+  private LLNode<SWASHBUCKLE> _front, _end;
+  private int _size;
+
+
+  // default constructor creates an empty queue
+  public RQueue()
+  {
+
+  }
+
+
+  public void enqueue( T enQVal )
+  {
+
+  }//O(?)
+
+
+  // remove and return thing at front of queue
+  // assume _queue ! empty
+  public T dequeue()
+  {
+
+  }//O(?)
+
+
+  public T peekFront()
+  {
+
+  }//O(?)
+
+
+  /***
+   * void sample() -- a means of "shuffling" the queue
+   * Algo:
+   *   < YOUR SUCCINCT SUMMARY HERE >
+   **/
+  public void sample ()
+  {
+
+  }//O(?)
+
+
+  public boolean isEmpty()
+  {
+    return _front == null;
+  } //O(?)
+
+
+  // print each node, separated by spaces
+  public String toString()
+  {
+
+  }//end toString()
+
+
+
+  //main method for testing
+  public static void main( String[] args )
+  {
+
+      /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
+    
+    Queue<String> PirateQueue = new RQueue<String>();
+
+    System.out.println("\nnow enqueuing..."); 
+    PirateQueue.enqueue("Dread");
+    PirateQueue.enqueue("Pirate");
+    PirateQueue.enqueue("Roberts");
+    PirateQueue.enqueue("Blackbeard");
+    PirateQueue.enqueue("Peter");
+    PirateQueue.enqueue("Stuyvesant");
+
+    System.out.println("\nnow testing toString()..."); 
+    System.out.println( PirateQueue ); //for testing toString()...
+
+    System.out.println("\nnow dequeuing..."); 
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+
+    System.out.println("\nnow dequeuing fr empty queue...\n" +
+                       "(expect NPE)\n"); 
+    System.out.println( PirateQueue.dequeue() );
+
+      ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
+
+  }//end main
+
+}//end class RQueue
