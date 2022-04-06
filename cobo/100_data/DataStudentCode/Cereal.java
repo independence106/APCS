5cdd38fff9e781063e18cd226f9bdf8d2ea4329a
yang zhou <independence106@gmail.com>
2022-04-05 21:43:23 -0400

    done for today

--- cobo/100_data/DataStudentCode/Cereal.java
+++ cobo/100_data/DataStudentCode/Cereal.java
@@ -1,3 +1,169 @@
+import javax.print.DocFlavor.STRING;
+
 public class Cereal {
-    
+    private String name;
+    private String type;
+
+    private double calories;
+    private double protein;
+    private double fat;
+    private double sodium;
+    private double fiber;
+    private double carbs;
+    private double sugar;
+    private double potassium;
+    private double vitamins;
+    private double shelf;
+    private double weight;
+    private double cups;
+    private double rating;
+
+    // this is optional you don't have to use this constructor
+    public Cereal(String name, String type, double calories, double protein, double fat, double sodium, double fiber, double carbs, double sugar, double potassium, double vitamins, double shelf, double weight, double cups, double rating) {
+        this.name = name;
+        this.type = type;
+        this.calories = calories;
+        this.protein = protein;
+        this.fat = fat;
+        this.sodium = sodium;
+        this.fiber = fiber;
+        this.carbs = carbs;
+        this.sugar = sugar;
+        this.potassium = potassium;
+        this.vitamins = vitamins;
+        this.shelf = shelf;
+        this.weight = weight;
+        this.cups = cups;
+        this.rating = rating;
+    }
+
+    /** Assignment Methods */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setCalories(double calories) {
+        this.calories= calories;
+    }
+
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    public void setSodium(double sodium) {
+        this.sodium = sodium;
+    }
+
+    public void setFiber(double fiber) {
+        this.fiber = fiber;
+    }
+
+    public void setCarbs(double carbs) {
+        this.carbs = carbs;
+    }
+
+    public void setSugar(double sugar) {
+        this.sugar = sugar;
+    }
+
+    public void setPotassium(double potassium) {
+        this.potassium = potassium;
+    }
+
+    public void setVitamins(double vitamins) {
+        this.vitamins = vitamins;
+    }
+
+    public void setShelf(double shelf) {
+        this.shelf = shelf;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setCups(double cups) {
+        this.cups = cups;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    /** Accessor Methods */
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public double getCalories() {
+        return this.calories;
+    }
+
+    public double getProtein() {
+        return this.protein;
+    }
+
+    public double getFat() {
+        return this.fat;
+    }
+
+    public double getSodium() {
+        return this.sodium;
+    }
+
+    public double getFiber() {
+        return this.fiber;
+    }
+
+    public double getCarbs() {
+       return this.carbs;
+    }
+
+    public double getSugar() {
+        return this.sugar;
+    }
+
+    public double getPotassium() {
+        return this.potassium;
+    }
+
+    public double getVitamins() {
+        return this.vitamins;
+    }
+
+    public double getShelf() {
+        return this.shelf;
+    }
+
+    public double getWeight() {
+       return this.weight;
+    }
+
+    public double getCups() {
+        return this.cups;
+    }
+
+    public double getRating() {
+        return this.rating;
+    }
+
+    public static void main(String[] args) {
+        Cereal appleJacks = new Cereal("Apple Jacks", "C", 110, 2, 1, 0, 125, 1, 11, 12, 12, 12, 12, 13, 20);
+        Cereal Fruitloops = new Cereal("Fruit Loops", "C", 110, 2, 1, 0, 125, 1, 11, 12, 12, 12, 12, 13, 20);
+        Cereal oatmeal = new Cereal("oatmeal", "C", 110, 2, 1, 0, 125, 1, 11, 12, 12, 12, 12, 13, 20);
+    }
+
 }
--- cobo/100_data/DataStudentCode/WeatherStation.java
+++ cobo/100_data/DataStudentCode/WeatherStation.java
@@ -6,30 +6,30 @@ public class WeatherStation {
    private String name;
    private String id;
    private String state;
-   private double lat;
-   private double lng;
-   
+   public double lat;
+   public double lng;
+
    WeatherStation(String name, String id, String state, double lat, double lng) {
       this.name = name;
       this.id = id;
       this.lat = lat;
       this.lng = lng;
-      this.state = state;   
+      this.state = state;
    }
-   
+
    /* Produce the id of this station */
-   public String getId() { 
+   public String getId() {
       return id;
    }
-   
+
    /* Produce the name of this station */
-   public String getName() { 
+   public String getName() {
       return name;
    }
-   
+
    /* Determine if this weather station is located in the given state */
    public boolean isLocatedInState(String st) {
       return this.state.equals(st);
    }
-   
+
 }
